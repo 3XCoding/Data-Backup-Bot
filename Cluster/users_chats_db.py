4ded0998a77477dadf2bd7ed6f0122b0209ce8c2
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-15 19:31:58 +0530

    Update users_chats_db.py

--- Cluster/users_chats_db.py
+++ Cluster/users_chats_db.py
@@ -1,6 +1,6 @@
 # https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
 import motor.motor_asyncio
-from info import DATABASE_NAME, DATABASE_URI
+from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT
 
 class Database:
     
@@ -22,10 +22,11 @@ class Database:
         )
 
 
-    def new_group(self, id, title):
+    def new_group(self, id, title, username):
         return dict(
             id = id,
             title = title,
+            username = username,
             chat_status=dict(
                 is_disabled=False,
                 reason="",
@@ -85,17 +86,14 @@ class Database:
     
 
 
-    async def add_chat(self, chat, title):
-        chat = self.new_group(chat, title)
+    async def add_chat(self, chat, title, username):
+        chat = self.new_group(chat, title, username)
         await self.grp.insert_one(chat)
     
 
     async def get_chat(self, chat):
         chat = await self.grp.find_one({'id':int(chat)})
-        if not chat:
-            return False
-        else:
-            return chat.get('chat_status')
+        return False if not chat else chat.get('chat_status')
     
 
     async def re_enable_chat(self, id):
@@ -104,6 +102,25 @@ class Database:
             reason="",
             )
         await self.grp.update_one({'id': int(id)}, {'$set': {'chat_status': chat_status}})
+        
+    async def update_settings(self, id, settings):
+        await self.grp.update_one({'id': int(id)}, {'$set': {'settings': settings}})
+        
+    
+    async def get_settings(self, id):
+        default = {
+            'button': SINGLE_BUTTON,
+            'botpm': P_TTI_SHOW_OFF,
+            'file_secure': PROTECT_CONTENT,
+            'imdb': IMDB,
+            'spell_check': SPELL_CHECK_REPLY,
+            'welcome': MELCOW_NEW_USERS,
+            'template': IMDB_TEMPLATE
+        }
+        chat = await self.grp.find_one({'id':int(id)})
+        if chat:
+            return chat.get('settings', default)
+        return default
     
 
     async def disable_chat(self, chat, reason="No Reason"):
