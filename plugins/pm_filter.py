5015b76921dd931a3f935c36c9f93ee194d9b065
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-16 09:27:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -28,20 +28,16 @@ SPELL_CHECK = {}
 
 
 
-@Client.on_message(filters.private & filters.text & ~filters.edited & filters.incoming)
+@Client.on_message(filters.private & filters.group filters.text & ~filters.edited & filters.incoming)
 async def give_filter(client, message):    
     await auto_filter(client, message)
 
-
-
-# # ---------- 🔘 [ | CALLBACKQUERY NEXT | ] 🔘 ---------- # #
-
-@Client.on_callback_query(filters.regex(r"^pnext"))
+@Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("mm okda 😁🙏", show_alert=True)
+        return await query.answer("oKda", show_alert=True)
     try:
         offset = int(offset)
     except:
