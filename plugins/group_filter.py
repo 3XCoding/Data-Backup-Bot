14ec208d71bfbb371f2bf90504df35c6425dbcd3
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-16 18:52:22 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -81,12 +81,12 @@ async def next_page(bot, query):
         ]
     btn.insert(0, 
         [
-            InlineKeyboardButton(f'🎟𝚂𝙴𝙰𝚁𝙲𝙷 𝙵𝙸𝙻𝙴 𝙽𝙰𝙼𝙴 : {search}', 'dupe')
+            InlineKeyboardButton(f'🗂 𝚂𝙴𝙰𝚁𝙲𝙷 𝙵𝙸𝙻𝙴 𝙽𝙰𝙼𝙴: {search}', 'dupe')
         ]
     )
     btn.insert(1,
         [
-            InlineKeyboardButton(f'🗂️𝙵𝙸𝙻𝙴𝚂 : {len(files)}', 'dupe'),
+            InlineKeyboardButton(f'📁 𝙵𝙸𝙻𝙴𝚂 : {len(files)}', 'dupe'),
             InlineKeyboardButton(f'💫 𝚃𝙸𝙿𝚂', 'tips')
         ]
     )
