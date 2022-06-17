092223204162ac198c00e9d0632bedffa6fdb56c
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-17 08:44:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -79,12 +79,12 @@ async def next_page(bot, query):
         ]
     btn.insert(0, 
         [
-            InlineKeyboardButton(f'🎟𝚂𝙴𝙰𝚁𝙲𝙷 𝙵𝙸𝙻𝙴 𝙽𝙰𝙼𝙴 : {search}', 'dupe')
+            InlineKeyboardButton(f'🗂 𝚈𝙾𝚄𝚁 𝙵𝙸𝙻𝙴 𝙽𝙰𝙼𝙴: {search}', 'dupe')
         ]
     )
     btn.insert(1,
         [
-            InlineKeyboardButton(f'🗂️𝙵𝙸𝙻𝙴𝚂 : {len(files)}', 'dupe'),
+            InlineKeyboardButton(f'📁 𝙵𝙸𝙻𝙴𝚂: {len(files)}', 'dupe'),
             InlineKeyboardButton(f'💫 𝚃𝙸𝙿𝚂', 'tips')
         ]
     )
