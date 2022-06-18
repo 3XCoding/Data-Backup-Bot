20d6d04a413ee8182db1eb75b694fcfda6786600
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-18 20:27:05 +0530

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -1,6 +1,6 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from database.connections_mdb import add_connection, all_connections, if_active, delete_connection
+from databasevs.connections_mdb import add_connection, all_connections, if_active, delete_connection
 from info import ADMINS
 import logging
 logger = logging.getLogger(__name__)
