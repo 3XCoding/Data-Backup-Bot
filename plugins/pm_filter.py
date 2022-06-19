f55dc31aedd7828fa084f2de9028454653b64404
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-19 21:00:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -393,6 +393,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         title = files.file_name
         size=get_size(files.file_size)
         f_caption = files.caption
+        if CUSTOM_FILE_CAPTION:
             try:
                 f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
                                                        file_size='' if size is None else size,
