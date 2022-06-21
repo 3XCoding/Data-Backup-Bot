597786720755a1ca9d45a4fb197bbfa9deb7b81b
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-21 07:04:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -668,18 +668,20 @@ async def auto_filter(client, msg, spoll=False):
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(IMDB_DELET_TIME)
             await hehe.delete()
+            await message.reply_text(text=f"", disable_notification = False)
+        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+            pic = imdb.get('poster')
+            poster = pic.replace('.jpg', "._V1_UX360.jpg")
+            hmm = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            await hmm.edit_text(text=f"", disable_notification = False)
         except Exception as e:
             logger.exception(e)
             fek = await message.reply_photo(photo="", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(IMDB_DELET_TIME)
-            await fek.delete()
+            await fek.edit_text(text=f"")
     else:
         fuk = await message.reply_photo(photo="", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(IMDB_DELET_TIME)
         await fuk.delete()
-        await message.reply_text(text=f"{search} Cʟᴏꜱᴇᴅ")
          
 
 async def advantage_spell_chok(msg):
