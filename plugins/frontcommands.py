aa73f4ee6d3c392daa868bac8052938a4a544028
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-21 13:47:52 +0530

    Update frontcommands.py

--- plugins/frontcommands.py
+++ plugins/frontcommands.py
@@ -187,8 +187,8 @@ async def style(c, m):
         cls = Fonts.strike
     if style == 'frozen':
         cls = Fonts.frozen
-    new_text = cls(m.message.reply_to_message.text)
+    new_text = cls<code>(m.message.reply_to_message.text)</code>
     try:
-        await m.message.edit_text<code>(new_text, reply_markup=m.message.reply_markup)</code>
+        await m.message.edit_text(new_text, reply_markup=m.message.reply_markup)
     except:
         pass
