7aca2bd5978780add4a084adc3f7face0d110f96
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-22 07:45:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,6 +51,20 @@ async def start(client, message):
             InlineKeyboardButton('⚙️ 𝚂𝙴𝚃𝚃𝙸𝙽𝙶𝚂', callback_data='help'),
             InlineKeyboardButton('💡 𝙰𝙱𝙾𝚄𝚃', callback_data='about')
         ]]
+          reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text="𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐"
+        )
+        await query.message.edit_text(
+            text="𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐."
+        )
+        await query.message.edit_text(
+            text="𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐. ."
+        )
+        await query.message.edit_text(
+            text="𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐. . ."
+        )
+        )
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
@@ -94,6 +108,20 @@ async def start(client, message):
             InlineKeyboardButton('⚙️ 𝚂𝙴𝚃𝚃𝙸𝙽𝙶𝚂', callback_data='help'),
             InlineKeyboardButton('💡 𝙰𝙱𝙾𝚄𝚃', callback_data='about')
         ]]
+          reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text="𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐"
+        )
+        await query.message.edit_text(
+            text="𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐."
+        )
+        await query.message.edit_text(
+            text="𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐. ."
+        )
+        await query.message.edit_text(
+            text="𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐. . ."
+        )
+        )
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
