c979ed52f8e5f513cafeb7783ba53086c59c82e6
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-22 13:00:31 +0530

    Update ping.py

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -5,7 +5,7 @@ import time
 import random
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
+from plugins.bharat.cust_p_filters import f_onw_fliter
 
 # -- Constants -- #
 ALIVE = "🙂" 
