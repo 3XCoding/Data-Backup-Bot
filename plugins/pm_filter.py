300ee4ddb411a17854393ae381cd72c2dbede3d9
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-22 20:08:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -3,7 +3,8 @@ import asyncio
 import re
 import ast
 
-from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty, \
+    make_inactive
 from Script import script
 import pyrogram
 from databasevs.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
@@ -11,7 +12,7 @@ from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GRO
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
-from utils import get_size, is_subscribed, get_poster, search_gagala, temp
+from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
 from databasevs.users_chats_db import db
 from databasevs.ia_filterdb import Media, get_file_details, get_search_results
 from databasevs.filters_mdb import(
