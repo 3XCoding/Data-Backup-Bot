2ff4c8792bf9867a01726c746d02e45260198c89
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-23 08:48:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -480,7 +480,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('𝙰𝚄𝙳𝙸𝙾-𝙱𝙾𝙾𝙺', callback_data='abook')
             ],[
             InlineKeyboardButton('𝚃𝙴𝚇𝚃 𝚃𝙾 𝚂𝙿𝙴𝙴𝙲𝙷', callback_data='ttss'),
-            InlineKeyboardButton('𝙿𝙸𝙽𝙶', callback_data='pings')
+            InlineKeyboardButton('𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙿𝙷', callback_data='telegraph)
             ],[
             InlineKeyboardButton('🛕 𝙷𝙾𝙼𝙴', callback_data='start'),
             InlineKeyboardButton('🖱 𝚂𝚃𝙰𝚃𝚄𝚂', callback_data='stats'),
@@ -624,13 +624,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-    elif query.data == "pings":
+    elif query.data == "telegraph":
         buttons = [[
             InlineKeyboardButton('❮ 𝙱𝙰𝙲𝙺', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.PINGS_TXT,
+            text=script.TELE_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
