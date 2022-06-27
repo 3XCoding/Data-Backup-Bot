d42ba3b29352e57b63f00e05aa706693ab19c0a6
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-06-27 12:01:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -971,7 +971,7 @@ async def advantage_spell_chok(msg):
             k = await query.message.edit('This Movie Not Found In DataBase')
             await asyncio.sleep(10)
             await k.delete()
-        )
+
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
