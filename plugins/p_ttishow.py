ed2c56a0733c0fddcaf3b676771375f69fda8cee
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-07-09 16:27:48 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -59,7 +59,7 @@ async def save_group(bot, message):
                                                                                             ],[
                                                                                             InlineKeyboardButton("⭐️ 𝚃𝙾𝚁𝚁𝙴𝙽𝚃 𝙶𝚁𝙾𝚄𝙿", url="https://t.me/BharatTorrentPro")
          
-       )                                                                                 ]]),
+        )                                                                                 ]]),
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
