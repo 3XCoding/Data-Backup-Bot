6f31c9b86163ee6fbe9d345c465a6edb3462432b
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-07-09 11:49:35 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -56,7 +56,8 @@ async def save_group(bot, message):
                                                  photo=(MELCOW_PIC),
                                                  caption=(MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  buttons = [[
-                                                     InlineKeyboardButton("🧑🏻‍⚖️ 𝙱𝙾𝚃 𝚁𝚄𝙻𝙴𝚂", url="https://telegra.ph/Hey-Natasha-Bot-07-07"),
+                                                     InlineKeyboardButton("🧑🏻‍⚖️ 𝙱𝙾𝚃 𝚁𝚄𝙻𝙴𝚂", url="https://telegra.ph/Hey-Natasha-Bot-07-07")
+                                                     ],[
                                                      InlineKeyboardButton("⭐️ 𝚃𝙾𝚁𝚁𝙴𝙽𝚃 𝙶𝚁𝙾𝚄𝙿", url="https://t.me/BharatTorrentPro")
                                                  ]]
                                                  reply_markup = InlineKeyboardMarkup(buttons)
@@ -64,7 +65,6 @@ async def save_group(bot, message):
                                                  reply_markup=reply_markup,
                                                  parse_mode='html'
         )
-
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
