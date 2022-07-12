dcc342a2ddc756a48cb076ce214fee8b79bfd253
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-07-12 15:44:23 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config(object):
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+    # watermark file
+    CHANNEL_URL = ""
+    
