34487d1338d451ccebbd735bc0c2b5aacb69e416
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-07-12 13:52:59 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -18,7 +18,7 @@ else:
     from info import info
 
 # the Strings used for this "thing"
-from Script import script
+from translation import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
