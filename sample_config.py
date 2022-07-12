fb6b149a6d2a77c362fc5a0b82aa22c286b0529a
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-07-12 13:50:55 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,1 +1,8 @@
-
+class Translation(object):
+    IFLONG_FILE_NAME = "File Name limit allowed by Telegram is {alimit} characters.\nThe given file name has {num} characters.\n<b>Essays Not allowed in Telegram file name!</b>\nPlease short your file name and try again!"""
+    DOWNLOAD_START = "<b><i>📥ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...</i></b>"
+    SAVED_RECVD_DOC_FILE = "<b>Document Downloaded Successfully.</b>"
+    RENAME_403_ERR = "<b>Sorry. You Are Not Permitted To Rename This File.</b>"
+    UPLOAD_START = "<b><i>📤ᴜᴘʟᴏᴀᴅɪɴɢ...</i></b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b><i>📥ᴅᴏᴡɴʟᴏᴀᴅ ᴄᴏᴍᴘʟᴇᴛᴇ...</b></i>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Downloaded in {} seconds. \nUploaded in {} seconds."
