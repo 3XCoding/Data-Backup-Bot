d6fe373b0f65f8547aa4c2b33be3309d77ff7df8
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-07-13 12:20:21 +0530

    Delete Config.py

--- Config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
-
-    # watermark file
-    CHANNEL_URL = ""
-    
