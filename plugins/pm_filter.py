a061f8585d9b1473b48aca6961e44c9ace5dc8fa
Hey Adithya <80088944+HeyAdithya@users.noreply.github.com>
2022-07-25 20:24:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -17,7 +17,6 @@ from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GRO
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
-from dyno import HEROKU_API_KEY
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
 from databasevs.users_chats_db import db
 from databasevs.ia_filterdb import Media, get_file_details, get_search_results
